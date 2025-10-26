36cec2aff3f19a4c5ae07c9df53a674d99c88ec1
MD RAHIM KHAN <abdurrahimkhan214dc@gmail.com>
2025-10-26 18:36:28 +0600

    add

--- Node.js and Angular/Frontend(Angular)/courier-service-angular/src/app/services/address-service.ts
+++ Node.js and Angular/Frontend(Angular)/courier-service-angular/src/app/services/address-service.ts
@@ -1,8 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Address } from '../model/address.model';
+import { environment } from '../api/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressService {
-  
+
+  private apiUrl=environment.apiUrl;
+
+    constructor(
+    private http: HttpClient,
+    private router: Router
+
+  ) { }
+
+    // Fetch all addresses
+  getAllAddresses(): Observable<Address[]> {
+    return this.http.get<Address[]>(this.apiUrl+"/api/address/");
+  }
+
+  // Save new address
+  saveAddress(address: Address): Observable<Address> {
+    return this.http.post<Address>(this.apiUrl+"/api/address/", address);
+  }
+
 }
--- Node.js and Angular/Frontend(Angular)/courier-service-angular/src/app/services/country-service.ts
+++ Node.js and Angular/Frontend(Angular)/courier-service-angular/src/app/services/country-service.ts
@@ -3,18 +3,33 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from '../api/environment';
 import { Observable } from 'rxjs';
+import { Country } from '../model/country.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
   
-private apiurl=environment.apiUrl;
+private apiUrl=environment.apiUrl;
 
 constructor(
   private http:HttpClient,
   private router:Router
 ){}
+
+
+
+  saveCountry(country: Country): Observable<Country> {
+
+    return this.http.post<Country>(this.apiUrl+"/api/countries/", country);
+
+  }
+
+  getAllCountry():Observable<Country[]>{
+
+    return this.http.get<Country[]>(this.apiUrl+"/api/countries/");
+
+  }
 
 
 // saveCountry(country:any): Observable<any> {
